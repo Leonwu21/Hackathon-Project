e0e920401fb5eed486bc5d1a14a3f268ae9dde77
Leon Wu <21leonwu@gmail.com>
2019-11-30 14:51:49 -0800

    added firestore config

---
+++ scripts/firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnubsXusBPP_EQ_oViSy9vM3vOY2dVSIw",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
